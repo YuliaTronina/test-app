592f2dce2f12d1882b727ffade1f5d2a5a4830e8
YuliaTronina <electron_87@mail.ru>
2022-10-18 10:31:32 +0500

    First_commit

---
+++ src/main/java/com/example/testapp/controller/MyController.java
@@ -0,0 +1,23 @@
+package com.example.testapp.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class MyController {
+
+    @GetMapping ("/hello")
+    public String showMyFirstPage (@RequestParam (name = "name", required = false,defaultValue = "World")
+                                   String name,
+                                   Model model){
+        model.addAttribute("name",name);
+        return  "my-first-page";
+
+    }
+
+
+}
